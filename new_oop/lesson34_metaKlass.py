3861a3eec0c87d5345a7bcd17f5d5620d60ab358
Anton <levantonio@mail.ru>
2023-01-26 12:36:34 +0300

    метаклассы

---
+++ new_oop/lesson34_metaKlass.py
@@ -0,0 +1,9 @@
+# class Point:
+#     MAX_COORD = 100
+#     MIN_COORD = 0
+
+
+A = type("Point", (), {"MAX_COORD": 100, "MIN_COORD": 0})
+"""
+Таким образом можно создать класс Point, который содержит поля MAX_COORD и MIN_COORD.
+"""
